686fd3b9cced102681b296246f167a439a4eaa5f
Henry Fung <henryfung3a27@yahoo.com.hk>
2020-03-27 23:46:56 +0000

    Moved custom-set-variables and custom-set-faces to custom.el

---
+++ custom.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(custom-safe-themes
+   (quote
+    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "ae65ccecdcc9eb29ec29172e1bfb6cadbe68108e1c0334f3ae52414097c501d2" default)))
+ '(inhibit-startup-screen t)
+ '(package-selected-packages
+   (quote
+    (smart-mode-line opam-user-setup use-package esup moe-theme neotree helm-swoop fish-mode helm magit multiple-cursors markdown-preview-mode markdown-mode+ flymd markdown-mode web-mode php-mode))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
